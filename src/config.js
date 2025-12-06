5740a2d44b219e3c287c804482dd210cc820095c
Prasenjit Santra (Ankush) <164280246+Ankush26030@users.noreply.github.com>
2025-12-07 00:38:30 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  token: 'MTQzMzE3MTQyODMzNjk5MjMzOA.G2cG4N.didhSbhNFpbKB4lw7-c7WGMbJ-JJ2MAmY_Jew8', // your discord bot token
+  clientId: "1234592539324059709",
+  prefix: '+', // bot prefix
+  ownerID: ['239496212699545601', '', '', '', '' ], //your discord id
+  SpotifyID: 'e6f84fbec2b44a77bf35a20c5ffa54b8', // spotify client id
+  SpotifySecret: '498f461b962443cfaf9539c610e2ea81', // spotify client secret
+  ankushcolor: '#ff0000', // embed colour
+  bugReportChannel: "1365060976604610590", // ID of the channel where bug reports will be sent
+  embedColor: '#ff0000', // Using your existing ankushcolor
+  supportServer: "https://discord.com/invite/w77ymEU82a", // Your support server link
+  
+  nodes: [
+    {
+      url: `lavalinkv4.serenetia.com:80`,
+      name: `NOTHING`,
+      auth: `https://dsc.gg/ajidevserver`,
+      secure: false
+    }
+  ],
+};
+
