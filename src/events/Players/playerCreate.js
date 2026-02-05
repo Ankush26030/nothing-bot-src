ac0f5b89129e285d11433f78abcb1adb4554027a
Prasenjit Santra (Ankush) <164280246+Ankush26030@users.noreply.github.com>
2026-02-06 02:14:38 +0530

    Remove sensitive webhook URL from playerCreate.js
    Removed the webhook URL from playerCreate.js for security.

--- src/events/Players/playerCreate.js
+++ src/events/Players/playerCreate.js
@@ -1,5 +1,5 @@
 const { MessageActionRow, MessageButton, EmbedBuilder, WebhookClient } = require("discord.js");
-const web1 = new WebhookClient({ url: `https://discord.com/api/webhooks/1365060395856826399/jUNebsF14XmQET-QebFpHTdoPs4Ac2YU07zg1QDFMr5AS6pxz8_f_ks9oHglrNWcNeaw` });
+const web1 = new WebhookClient({ url: `` });
 const { KazagumoPlayer } = require("kazagumo")
 
 module.exports = {
